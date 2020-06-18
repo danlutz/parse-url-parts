0df526f31a3c1953261bfe89c0bfeb023fab1efe
Daniel Lutz <daniel-lutz@live.at>
2020-06-18 12:20:00 +0200

    Include typings

--- src/parseUrlParts.ts
+++ src/parseUrlParts.ts
@@ -1,4 +1,18 @@
-import { ParsedURL, URLProtocol } from "./typings/ParsedURL"
+export type URLProtocol = "http" | "https" | "ftp" | "mailto" | "file" | "data" | "irc"
+
+export interface ParsedURL {
+	protocol: URLProtocol
+	username?: string
+	password?: string
+	host: string
+	subDomains?: string
+	rootDomain: string
+	topLevelDomain?: string
+	port?: number
+	path?: string
+	queryParams?: string
+	fragment?: string
+}
 
 const urlRegex = /(\w+):\/\/((\w+):(\w+)@)?((([\w-]+)\.)*[\w-]+)?(:(\d+))?((\/[\w-]+)*)?(\?(((\w+)=(\w+)(&|;)?)+))?(#(\w+))?/
 
